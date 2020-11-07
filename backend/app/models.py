c4c581c0b0bdc3252a3526bd1b169ee2c7e15ffd
peterghrong <peter.gh.rong@gmail.com>
2020-11-07 17:46:07 -0500

    initial project domain model
    finished project domain model

--- backend/app/models.py
+++ backend/app/models.py
@@ -4,3 +4,29 @@ from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
 
 # create model classes here
+
+# Many-to-Many relationships requires helper table
+# According to the flask documentation
+contacts = db.Table('contacts',
+                    db.Column('contact_id', db.Integer, db.ForeignKey(
+                        'contact.id'), primary_key=True),
+                    db.Column('project_id', db.Integer, db.ForeignKey(
+                        'project.id'), primary_key=True)
+                    )
+# Project Model
+
+
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    address = db.Column(db.String(256))
+    city = db.Column(db.String(64))
+    province = db.Column(db.String(64))
+    postalCode = db.Column(db.String(64))
+    neighbourhood = db.Column(db.String(256))
+    year = db.Column(db.Integer)
+    name = db.Column(db.String(64))
+    type = db.Column(db.String(64))
+    # one-to-many relationship to Note
+    notes = db.relationship('Note', backref='project', lazy=True)
+    contacts = db.relationship(
+        'Contact', secondary=contacts, lazy='subquery', backref=db.backref('projects', lazy=True))
