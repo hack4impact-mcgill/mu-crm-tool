3851000575af0c058663ad51d8727d7f240d01d3
dgiltz <70062287+dgiltz@users.noreply.github.com>
2020-11-16 14:12:43 -0500

    Add Note
    Created Note class according to the domain model

--- backend/app/models.py
+++ backend/app/models.py
@@ -4,3 +4,15 @@ from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
 
 # create model classes here
+import datetime
+
+class Note(db.Model):
+    __tablename__ = "notes"
+    note_id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(256), nullable=False)
+    createdDateTime = db.Column(db.DateTime(), default=datetime.utcnow)
+    updatedDateTime = db.Column(db.DateTime(), default=datetime.utcnow, onupdate=datetime.utcnow)
+    # many to one relationship to Project
+    project_id = db.relationship(db.Integer, db.Foreign_Key('project.id'), nullable=False)
+    # many one to relationship to MuUser via created_by
+    created_by = db.relationship(db.Integer, db.Foreign_Key('muuser.id'), nullable=False)
