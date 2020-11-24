1a9c01044edae376ee498adeed755593e57d3337
aqghawa <59985051+aqghawa@users.noreply.github.com>
2020-11-24 09:11:51 -0500

    Black format

--- backend/app/models.py
+++ backend/app/models.py
@@ -4,21 +4,28 @@ from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy.dialects.postgresql import UUID
 import uuid
+
 # create model classes here
 
 # MuUser Domain Model Class
-class MuUser(db.Model):   
-    #Initializing Table
-    __tablename__='users'
-    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
+class MuUser(db.Model):
+    # Initializing Table
+    __tablename__ = "users"
+    id = db.Column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        default=uuid.uuid4,
+        unique=True,
+        nullable=False,
+    )
     name = db.Column(db.String(64), nullable=False)
     email = db.Column(db.String(64), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
     role = db.Column(db.String(32), unique=True, nullable=False)
-    #Relationships
+    # Relationships
     notes = db.relationship("Note", backref="user", lazy=True)
     donations = db.relationship("Donation", backref="user", lazy=True)
-    #Serialize class
+    # Serialize class
     @property
     def serialize(self):
         """Return object data in serializeable format"""
@@ -27,13 +34,15 @@ class MuUser(db.Model):
             "name": self.name,
             "password_hash": self.password_hash,
             "email": self.email,
-            "role": self.role
+            "role": self.role,
         }
+
     @staticmethod
     def serialize_list(users):
         json_users = []
         for user in users:
             json_users.append(user.serialize)
         return json_users
+
     def __repr__(self):
         return "<User %r>" % self.email
