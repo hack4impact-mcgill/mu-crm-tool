130d289811cd48b4f14e988bf50b9b0ee8021a83
aqghawa <59985051+aqghawa@users.noreply.github.com>
2020-11-24 08:37:14 -0500

    Removed Removed everything related to authentication, this includes imports, http_auth, and password properties. Added lazy=true.

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,28 +1,13 @@
 import json
 
-from . import db, http_auth
-from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
-from itsdangerous import BadSignature, SignatureExpired
+from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy.dialects.postgresql import UUID
 import uuid
 # create model classes here
 
-
-#Authorization for User
-@http_auth.verify_token
-def verify_auth_token(token):
-    s = Serializer(current_app.config["SECRET_KEY"])
-    try:
-        data = s.loads(token)
-    except SignatureExpired:
-        return False  # valid token, but expired
-    except BadSignature:
-        return False  # invalid token
-    user = MuUser.query.get(data["token"])
-    return user
-#MuUser Domain Model Class
-class MuUser(db.Model)    
+# MuUser Domain Model Class
+class MuUser(db.Model):   
     #Initializing Table
     __tablename__='users'
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
@@ -31,27 +16,8 @@ class MuUser(db.Model)
     password_hash = db.Column(db.String(128), nullable=False)
     role = db.Column(db.String(32), unique=True, nullable=False)
     #Relationships
-    notes = db.relationship("Note", backref="user")
-    donations = db.relationship("Donation", backref="user")
-    #Password Properties
-    @property
-    def password(self):
-        raise AttributeError("password is not a readable attribute")
-    @password.setter
-    def password(self, password):
-        self.password_hash = generate_password_hash(password)
-    def verify_password(self, password):
-        return check_password_hash(self.password_hash, password)
-    def change_password(self, old_password, new_password):
-        if not self.verify_password(old_password):
-            return False
-        self.password = new_password
-        db.session.add(self)
-        db.session.commit()
-        return True
-    def generate_auth_token(self, expiration=86400):
-        s = Serializer(current_app.config["SECRET_KEY"], expiration)
-        return s.dumps({"token": self.id})
+    notes = db.relationship("Note", backref="user", lazy=True)
+    donations = db.relationship("Donation", backref="user", lazy=True)
     #Serialize class
     @property
     def serialize(self):
