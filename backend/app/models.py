8850d3d53119eac3ee8a4e6a60fda643b7f6d9fb
aqghawa <59985051+aqghawa@users.noreply.github.com>
2020-11-24 09:25:58 -0500

    Edited event and addedBy column parameters

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,22 +1,24 @@
 import json
 
 from . import db
-from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy.dialects.postgresql import UUID
 import uuid
 import datetime
+
 # create model classes here
 
+
 class Donation(db.Model):
-    __tablename__='donations'
+    __tablename__ = "donations"
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     name = db.Column(db.String(64), nullable=False)
     email = db.Column(db.String(64), unique=True, nullable=False)
-    date = db.Column(db.DateTime(), default=datetime.utcnow)
+    date = db.Column(db.DateTime(), default=datetime.datetime.utcnow)
     donationSource = db.Column(db.String(128), nullable=False)
-    event = db.Column(db.String(128), nullable=False)
+    event = db.Column(db.String(128))
     numTickets = db.Column(db.Integer)
-    addedBy = db.Column(db.Integer, db.ForeignKey('user.id')
+    addedBy = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=True)
+
     @property
     def serialize(self):
         """Return object data in serializeable format"""
@@ -28,13 +30,15 @@ class Donation(db.Model):
             "donationSource": self.donationSource,
             "event": self.event,
             "numTickets": self.numTickets,
-            "addedBy": self.addedBy
+            "addedBy": self.addedBy,
         }
+
     @staticmethod
     def serialize_list(donations):
         json_donations = []
         for donation in donations:
             json_donations.append(donation.serialize)
         return json_donations
+
     def __repr__(self):
         return "<Donation %r>" % self.id
