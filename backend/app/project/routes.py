28e418a56eee3cdc12238e1248fbed75b7e3554f
peterghrong <peter.gh.rong@gmail.com>
2020-11-29 11:52:04 -0500

    Implemented get_projects_of_type
    Implemented a helper function that fetches all the possible types, and "get projects of a specified type"

--- backend/app/project/routes.py
+++ backend/app/project/routes.py
@@ -1,10 +1,30 @@
-from flask import Flask, abort
-app = Flask(__name__)
+from flask import abort, jsonify
+from app.models import Project, db
+from sqlalchemy import distinct
+from . import project
+
+# helper returns all project types;
+@project.route("/project/types", methods=["GET"])
+def get_all_project_types():
+    types = []
+    for type in Project.query.distinct(Project.type):
+        types.append(Project.type)
+    return jsonify(types)
 
 
-@app.route('/project/<project_name>')
-def get_proeject(project_name=None):
-    if project_name is None:
-        abort(404, "No item found with specified ID")
+# return projects with the specified type
+@project.route("/project/<type>", method=["GET"])
+def get_projects_of_type(type):
+    if type is None:
+        abort(404, "Project type invalid")
     else:
-        return 'do something'
+        # this returns None if the project type doesn't exist in the database
+        exists = db.session.query(Project.type).filter_by(type=type).scalar()
+        if exists is None:
+            abort(404, "Project type invalid")
+        else:
+            projects = Project.query.filter_by(type=type)
+            if projects is None:
+                abort(404, "No project found with specified project type")
+            else:
+                return jsonify(Project.serialize_list(projects))
