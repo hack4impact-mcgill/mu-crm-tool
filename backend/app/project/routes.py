fe341ef0a193c0650796e7a58450e1b0c469ac86
peterghrong <peter.gh.rong@gmail.com>
2020-11-29 12:02:55 -0500

    removed some redundent code

--- backend/app/project/routes.py
+++ backend/app/project/routes.py
@@ -18,13 +18,8 @@ def get_projects_of_type(type):
     if type is None:
         abort(404, "Project type invalid")
     else:
-        # this returns None if the project type doesn't exist in the database
-        exists = db.session.query(Project.type).filter_by(type=type).scalar()
-        if exists is None:
-            abort(404, "Project type invalid")
+        projects = Project.query.filter_by(type=type)
+        if projects is None:
+            abort(404, "No project found with specified project type")
         else:
-            projects = Project.query.filter_by(type=type)
-            if projects is None:
-                abort(404, "No project found with specified project type")
-            else:
-                return jsonify(Project.serialize_list(projects))
+            return jsonify(Project.serialize_list(projects))
