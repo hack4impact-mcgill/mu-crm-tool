5087f1e8109a8633b34c79a710fa530e03c6670c
dgiltz <70062287+dgiltz@users.noreply.github.com>
2020-12-10 21:54:36 -0500

    Black Reformatting - Part 3

--- backend/app/note/routes.py
+++ backend/app/note/routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, abort, jsonify, request
+rom flask import Flask, abort, jsonify, request
 from . import note
 from . import db
 from app.models import Note
@@ -8,18 +8,14 @@ from app.models import Note
 def edit_note(id):
     # description is only updatable field
     description = request.form["description"]
-    
+
     note = Note.query.filter_by(note_id=id).first()
     if note is None:
         abort(404, "No note found with specified id")
-    
     if description is not None:
         note.description = description
-   
     if description == "":
         abort(404, "Nothing to update")
-    
-    
     note.updatedDateTime = datetime.datetime.utcnow()
 
     return jsonify(note.serialize)
@@ -30,7 +26,6 @@ def delete_note(id):
     note = Note.query.filter_by(note_id=id).first()
     if note is None:
         abort(404, "No note found with specified id")
-
     db.session.delete(note)
     db.session.commit()
 
