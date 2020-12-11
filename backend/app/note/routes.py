d5bba437c6cad18b16dd55e86af6be654b881453
dgiltz <70062287+dgiltz@users.noreply.github.com>
2020-12-10 21:27:39 -0500

    Add Delete and Post endpoints to Note

---
+++ backend/app/note/routes.py
@@ -0,0 +1,40 @@
+from flask import Flask, abort, jsonify, request
+from . import note
+from . import db
+from app.models import Note
+
+# edit a note
+@note.route("/<note_uuid>/update", METHODS=["POST"])
+def edit_note(id):
+    # description is only updatable field
+    description = request.form["description"]
+    
+    note = Note.query.filter_by(note_id=id).first()
+    if note is None:
+        abort(404, "No note found with specified id")
+        
+    if description is not None:
+        note.description = description
+
+    if description == "":
+        abort(404, "Nothing to update")
+    
+    note.updatedDateTime = datetime.datetime.utcnow()
+
+    return jsonify(note.serialize)
+
+
+
+@note.route("/<note_uuid>", METHODS=["DELETE"])
+def delete_note(id):
+    note = Note.query.filter_by(note_id=id).first()
+    if note is None:
+        abort(404, "No note found with specified id")
+
+    db.session.delete(note)
+    db.session.commit()
+
+    return jsonify(note.serialize)
+   
+ 
+    
