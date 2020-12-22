495e59893b421a260c8b7cda678a136939901dcb
ahmad675 <ahmadq200124@gmail.com>
2020-12-22 12:36:25 -0500

    Add MuUser domain model class (#61)
    * Added MuUser domain model class
    
    * Changed id and class names to match domain model
    
    * Removed Removed everything related to authentication, this includes imports, http_auth, and password properties. Added lazy=true.
    
    * Removed an extra comma
    
    * Black format
    
    * Removed password_hash
    
    Co-authored-by: aqghawa <59985051+aqghawa@users.noreply.github.com>

--- backend/app/models.py
+++ backend/app/models.py
@@ -94,3 +94,42 @@ class ContactType(db.Model):
     type = db.Column(db.String(64))
     description = db.Column(db.String(512))
     contact = db.relationship("Contact", backref="contact_type", lazy=True)
+
+
+# MuUser Domain Model Class
+class MuUser(db.Model):
+    # Initializing Table
+    __tablename__ = "users"
+    id = db.Column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        default=uuid.uuid4,
+        unique=True,
+        nullable=False,
+    )
+    name = db.Column(db.String(64), nullable=False)
+    email = db.Column(db.String(64), unique=True, nullable=False)
+    role = db.Column(db.String(32), unique=True, nullable=False)
+    # Relationships
+    notes = db.relationship("Note", backref="user", lazy=True)
+    donations = db.relationship("Donation", backref="user", lazy=True)
+    # Serialize class
+    @property
+    def serialize(self):
+        """Return object data in serializeable format"""
+        return {
+            "id": self.id,
+            "name": self.name,
+            "email": self.email,
+            "role": self.role,
+        }
+
+    @staticmethod
+    def serialize_list(users):
+        json_users = []
+        for user in users:
+            json_users.append(user.serialize)
+        return json_users
+
+    def __repr__(self):
+        return "<User %r>" % self.email
