3d195df40f67c5be4d4aa4e5ac3e083a0bbb2b14
celinehuang <celine.huang@mail.mcgill.ca>
2021-01-08 19:19:04 -0800

    Test eslint job

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,14 +7,18 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+			        <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          							className="App-link"
           href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+			          target="_blank"
+    rel="noopener noreferrer"
+
+
+
+
         >
           Learn React
         </a>
