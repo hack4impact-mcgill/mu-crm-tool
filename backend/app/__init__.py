ab43a9d8ab5c9c431b42ee8bfb85bc884dbc05bd
CurtisMIT <curtismitmit@gmail.com>
2021-01-11 18:37:26 -0500

    psycopg2 fix attempt #3

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,17 +1,10 @@
 import os
 from flask import Flask
 from flask_cors import CORS
-from flask_sqlalchemy import SQLAlchemy as _BaseSQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 from config import config
-
-
-class SQLAlchemy(_BaseSQLAlchemy):
-    def apply_pool_defaults(self, app, options):
-        super(SQLAlchemy, self).apply_pool_defaults(app, options)
-        options["pool_pre_ping"] = True
-
 
 db = SQLAlchemy()
 migrate = Migrate()
