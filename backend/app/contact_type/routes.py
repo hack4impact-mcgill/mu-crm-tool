d97a82b832ab7354a34cf0e4efb2347fcc3d443d
carolaltimas <55726229+carolaltimas@users.noreply.github.com>
2021-02-01 22:52:57 -0500

    Making a separate branch for editing contact_type

--- backend/app/contact_type/routes.py
+++ backend/app/contact_type/routes.py
@@ -15,3 +15,31 @@ def delete_contact_type(id):
     db.session.commit()
 
     return jsonify(contact_type.serialize)
+
+# edit a contact_type by id
+@contact_type.route("/<uuid:id>/edit", methods=["PUT"])
+def edit_contact_type(id):
+    data = request.get_json(force=True)
+    hex_colour = data.get("hex_colour")
+    type = data.get("type")
+    description = data.get("description")
+
+    contact_type = ContactType.query.filter_by(id=id).first()
+    if contact_type is None:
+        abort(404, "No contact type found with specified ID.")
+
+    if hex_colour is not None:
+        contact_type.hex_colour = hex_colour
+
+    if type is not None:
+        contact_type.type = type
+
+    if description is not None:
+        contact_type.description = description
+
+    if not data:
+        abort(400, "No fields to update.")
+
+    db.session.add(contact_type)
+    db.session.commit()
+    return jsonify(contact_type.serialize)
--- backend/tests/test_contact_type.py
+++ backend/tests/test_contact_type.py
@@ -19,7 +19,7 @@ class ContactTypeTestCase(unittest.TestCase):
         db.drop_all()
         self.app_context.pop()
 
-    def test_contact_type_routes(self):
+    def test_contact_type_route(self):
         ct_id = uuid.uuid4()
         ct = ContactType(
             id=ct_id,
@@ -37,3 +37,45 @@ class ContactTypeTestCase(unittest.TestCase):
         # deleting a contact_type that does not exist
         response = self.client.delete("/contact_type/{}".format(ct_id))
         self.assertEqual(response.status_code, 404)
+
+def test_edit_contact_type_route(self):
+        ct_id = uuid.uuid4()
+        ct = ContactType(
+            id=ct_id,
+            hex_colour="#C414C7",
+            type="dummy type",
+            description="dummy description",
+        )
+        db.session.add(ct)
+        db.session.commit()
+
+        # update a contact_type with empty arguments
+        response = self.client.put("/contact_type/{}/edit".format(id),
+            content_type="application/json",
+            data=json.dumps({}),
+        )
+        self.assertEqual(response.status_code, 400)
+
+        # new arguments for contact_type
+        edited_ct = {
+            "hex_colour": "#ffffff",
+            "type": "new dummy type",
+            "description": "new dummy description",
+        }
+
+        # edit a contact_type does exist
+        response = self.client.put("/contact_type/{}/edit".format(ct_id),
+            content_type="application/json",
+            data=json.dumps(edited_ct)
+
+        )
+        self.assertEqual(response.status_code, 200)
+
+        # editing a contact_type that doesn't exist
+        response = self.client.put(
+            "/contact_type/{}/edit".format(uuid.uuid4()),
+            content_type="application/json",
+            data=json.dumps(edited_ct),
+        )
+        self.assertEqual(response.status_code, 404)
+        
