e8be81f49db41300c5d5fe05f758d1b52a087021
dgiltz <70062287+dgiltz@users.noreply.github.com>
2021-02-02 11:23:10 -0500

    Add Edit Contact Route

--- backend/app/contact/routes.py
+++ backend/app/contact/routes.py
@@ -3,6 +3,59 @@ from app import db
 from app.models import Contact
 from . import contact
 
+# Edit a contact by ID
+@contact.route("/<uuid:id>/edit", methods=["PUT"])
+def edit_contact(id):
+    contact = Contact.query.filter_by(id=id).first()
+    if contact is None:
+        abort(404, "No contact found with specified id")
+
+    data = request.get_json(force=True)
+    name = data.get("name")
+    email = data.get("email")
+    secondary_email = data.get("secondary_email")
+    cellphone = data.get("cellphone")
+    role = data.get("role")
+    organization = data.get("organization")
+    neighbourhood = data.get("neighbourhood")
+    projects = data.get("projects")
+    contact_type = data.get("contact_type")
+
+    if name is not None:
+        contact.name = name
+
+    if email is not None:
+        contact.email = email
+
+    if secondary_email is not None:
+        contact.secondary_email = secondary_email
+
+    if cellphone is not None:
+        contact.cellphone = cellphone
+
+    if role is not None:
+        contact.role = role
+
+    if organization is not None:
+        contact.organization = organization
+
+    if neighbourhood is not None:
+        contact.neighbourhood = neighbourhood
+
+    if projects is not None:
+        contact.projects = projects
+
+    if contact is not None:
+        contact.contact_type = contact.contact_type
+
+    if not data:
+        abort(400, "No feilds to update")
+    
+    db.session.add(contact)
+    db.session.commit()
+
+    return jsonify(contact.serialize)
+
 # delete a contact by id
 @contact.route("/<uuid:id>", methods=["DELETE"])
 def delete_contact_type(id):
@@ -14,3 +67,4 @@ def delete_contact_type(id):
     db.session.commit()
 
     return jsonify(contact.serialize)
+
