1afc67452e0aed6892e29326716c64c5987d4044
peterghrong <peter.gh.rong@gmail.com>
2021-02-13 18:17:06 -0500

    get donation by donor email

--- backend/app/models.py
+++ backend/app/models.py
@@ -11,7 +11,7 @@ class Donation(db.Model):
     __tablename__ = "donations"
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     name = db.Column(db.String(64), nullable=False)
-    email = db.Column(db.String(64), unique=True, nullable=False)
+    email = db.Column(db.String(64), nullable=False)
     date = db.Column(db.DateTime(), default=datetime.datetime.utcnow)
     donation_source = db.Column(db.String(128), nullable=False)
     event = db.Column(db.String(128))
