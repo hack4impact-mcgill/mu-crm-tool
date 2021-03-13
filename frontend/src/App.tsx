e505a3daad7efcfb9f1a9a795c8723ed9c25502a
peterghrong <peter.gh.rong@gmail.com>
2021-03-13 12:06:55 -0500

    Initial

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,20 +5,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* render the projects into the two different sections and 
+      display the project name, borough and project type 
+      with the last two using 
+      https://material-ui.com/components/chips/#chip like in the 
+      figma prototype */}
+      <h1>Landing page</h1>
+
+      <div className="In Progress">
+          <h1>In Progress</h1>
+      </div>
+      <div className="Completed">
+        <h1>Completed</h1>
+      </div>
+
     </div>
   );
 }
---
+++ frontend/src/App/components/InProgress.tsx
@@ -0,0 +1,12 @@
+class HelloMessage extends React.Component{
+    render() {
+        return (
+            <div>
+                Hello
+            </div>
+        )
+    }
+}
+ReactDOM.render(
+    <HelloMessage name="taylor">
+)
