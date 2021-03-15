d0de80c4357b37f9141135ff51d84d1174f8919c
CurtisMIT <curtismitmit@gmail.com>
2021-03-15 01:01:09 -0400

    useless renaming

--- frontend/src/components/pages/home/index.tsx
+++ frontend/src/components/pages/home/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const LandingPage: React.FC = () => {
+const Home: React.FC = () => {
     return (
         <div>
             HI
@@ -8,4 +8,4 @@ const LandingPage: React.FC = () => {
     )
 }
 
-export default LandingPage;
+export default Home;
