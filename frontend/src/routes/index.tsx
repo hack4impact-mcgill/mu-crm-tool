29c396a0802f2ed1f90d21649f6718942a1e8595
peterghrong <peter.gh.rong@gmail.com>
2021-03-17 23:09:24 -0400

    Just to get started

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -3,7 +3,8 @@ import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 
 export enum RouteName {
-    HOME = '/'
+    HOME = '/',
+    PROJECTS = '/projects'
 }
 
 
@@ -11,12 +12,21 @@ const Home = loadable(() => import('../components/pages/home'), {
     fallback: <div>Loading...</div>
 })
 
+const Projects = loadable(() => import('../components/pages/projects'), {
+    fallback: <div>Loading...</div>
+})
+
+
 export const Routes: React.FC = () => {
     return (
         <React.Fragment>
             <Switch>
-                <Route path={RouteName.HOME}>
+                <Route exact path={RouteName.HOME}>
                     <Home />
+                </Route>
+
+                <Route path={RouteName.PROJECTS}>
+                    <Projects />
                 </Route>
             </Switch>
         </React.Fragment>
