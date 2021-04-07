3438492aff938d116daef7d0a1539d095c6cc918
dgiltz <70062287+dgiltz@users.noreply.github.com>
2021-04-07 11:52:40 -0400

    Remove extraneous add a contact code

--- backend/app/contact/routes.py
+++ backend/app/contact/routes.py
@@ -3,43 +3,6 @@ from app import db
 from app.models import Contact
 from . import contact
 
-# create a contact
-@contact.route("", methods=["POST"])
-def create_contact():
-    data = request.get_json(force=True)
-    name = data.get("name")
-    email = data.get("email")
-    secondary_email = data.get("secondary_email")
-    cellphone = data.get("cellphone")
-    role = data.get("role")
-    organization = data.get("organization")
-    neighbourhood = data.get("neighbourhood")
-    projects = data.get("projects")
-
-    if (
-        # all non-nullable attributes and relationships
-        name == ""
-        or email == ""
-        or cellphone == ""
-        or organization == ""
-        or projects == None
-    ):
-        abort(400, "Indicated fields cannot be empty")
-    new_contact = Contact(
-        name=name,
-        email=email,
-        secondary_email=secondary_email,
-        cellphone=cellphone,
-        role=role,
-        organization=organization,
-        neighbourhood=neighbourhood,
-        projects=projects,
-    )
-
-    db.session.add(new_contact)
-    db.session.commit()
-
-
 # delete a contact by id
 @contact.route("/<uuid:id>", methods=["DELETE"])
 def delete_contact_type(id):
