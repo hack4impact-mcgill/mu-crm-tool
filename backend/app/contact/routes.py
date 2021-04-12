9b9fb553191643556148a4fd9539b07511954ff2
CurtisMIT <41308927+CurtisMIT@users.noreply.github.com>
2021-04-12 13:21:00 -0400

    Get a contact's contact types (#152)
    * added forgotten migration and route and test for getting contact's contact types
    
    * black
    
    * black 2

--- backend/app/contact/routes.py
+++ backend/app/contact/routes.py
@@ -1,6 +1,6 @@
 from flask import jsonify, abort
 from app import db
-from app.models import Contact
+from app.models import Contact, ContactType
 from . import contact
 
 # delete a contact by id
@@ -14,3 +14,13 @@ def delete_contact_type(id):
     db.session.commit()
 
     return jsonify(contact.serialize)
+
+
+# get a contact's contact types
+@contact.route("/<uuid:id>/contact-types", methods=["GET"])
+def get_contact_contact_types(id):
+    contact = Contact.query.filter_by(id=id).first()
+    if contact is None:
+        abort(404, "No contact found with specified ID.")
+
+    return jsonify(ContactType.serialize_list(contact.contact_types))
