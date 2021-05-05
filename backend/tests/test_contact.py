f66101999026c339aa453f79f8a3152a2dde7600
dgiltz <giltzdeclan@gmail.com>
2021-05-05 14:54:07 -0400

    copying over route

--- backend/tests/test_contact.py
+++ backend/tests/test_contact.py
@@ -100,3 +100,95 @@ class ContactTestCase(unittest.TestCase):
         actual_ids = [json_response[0]["id"], json_response[1]["id"]]
         expected_ids = [str(ct0_id), str(ct1_id)]
         self.assertCountEqual(expected_ids, actual_ids)
+
+# test edit a contact
+def test_edit_a_contact(self):
+        dummy_ct_id = uuid.uuid4()
+        dummy_ct = ContactType(
+            id=dummy_ct_id,
+            hex_colour="#ffffff",
+            type="dummy type",
+            description="dummy description",
+        )
+
+        c_id = uuid.uuid4()
+        c = Contact(
+            id=c_id,
+            name="dummy name",
+            email="dummy email",
+            secondary_email="dummy secondary email",
+            cellphone="dummy cellphone",
+            role="dummy role",
+            organization="dummy organization",
+            neighbourhood="dummy neighbourhood",
+        )
+
+        dummy_ct_id = uuid.uuid4()
+        dummy_ct = ContactType(
+            id=dummy_ct_id,
+            hex_colour="#ffffff",
+            type="dummy type",
+            description="dummy description",
+            contacts=[c],
+        )
+
+        db.session.add(dummy_ct)
+        db.session.commit()
+
+        # Ensuring contact is added for contact_type
+        contact_type = ContactType.query.filter_by(id=dummy_ct_id).first()
+        self.assertEqual(len(contact_type.contacts), 1)
+
+        update_obj = {
+            "name": "edited name",
+            "email": "edited email",
+            "secondary_email": "edited secondary email",
+            "cellphone": "edited cellphone",
+            "role": "edited role",
+            "organization": "edited organization",
+            "neighbourhood": "edited neighbourhood",
+        }
+
+        # edit a contact with valid arguments
+        response = self.client.put(
+            "/contact/{}".format(c_id),
+            content_type="application/json",
+            data=json.dumps(update_obj),
+        )
+        self.assertEqual(response.status_code, 200)
+        json_response = json.loads(response.get_data(as_text=True))
+        self.assertDictContainsSubset(update_obj, json_response)
+
+        empty_obj = {
+            "name": "",
+            "email": "",
+            "secondary_email": "",
+            "cellphone": "",
+            "role": "",
+            "organization": "",
+            "neighbourhood": "",
+        }
+
+        # edit a contact with invalid arguments
+        response = self.client.put(
+            "/contact/{}".format(c_id),
+            content_type="application/json",
+            data=json.dumps(empty_obj),
+        )
+        self.assertEqual(response.status_code, 400)
+
+        # edit a contact with an empty dict
+        response = self.client.put(
+            "/contact/{}".format(c_id),
+            content_type="application/json",
+            data=json.dumps({}),
+        )
+        self.assertEqual(response.status_code, 400)
+
+        # edit a contact that does not exist
+        response = self.client.put(
+            "/contact/{}/edit".format(uuid.uuid4()),
+            content_type="application/json",
+            data=json.dumps({}),
+        )
+        self.assertEqual(response.status_code, 404)
