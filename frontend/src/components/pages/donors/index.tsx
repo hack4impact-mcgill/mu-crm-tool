f1a7cfa796e309fa26d611f4aed30fe04669c8ae
Jerry Xia <jerrylxia@gmail.com>
2021-05-07 16:47:18 -0400

    Added some basic pagination variables

--- frontend/src/components/pages/donors/index.tsx
+++ frontend/src/components/pages/donors/index.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
+function clickMe() {
+  alert("You clicked me!");
+}
 const DonorPage: React.FC = () => {
-  return <div>donors placeholder</div>;
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const [postsPerPage, setPostsPerPage] = useState(10);
+
+  //setPosts(data here)
+
+  return (
+    <div>
+      <button onClick={clickMe}>Button</button>
+    </div>
+  );
 };
 
 export default DonorPage;
