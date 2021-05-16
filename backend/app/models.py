73aa431372942b3a586c0a101e755357dc5225f3
CurtisMIT <41308927+CurtisMIT@users.noreply.github.com>
2021-05-16 16:43:11 -0400

    populate db (#173)
    * populate db
    
    * black

--- backend/app/models.py
+++ backend/app/models.py
@@ -3,6 +3,24 @@ from . import db
 from sqlalchemy.dialects.postgresql import UUID
 import uuid
 import datetime
+from datetime import date
+import random
+import string
+
+
+def random_bool():
+    return random.choice([True, False])
+
+
+def random_str(n):
+    return "".join(random.choices(string.ascii_letters + string.digits, k=n))
+
+
+def random_date():
+    start = date.today().replace(day=1, month=1).toordinal()
+    end = date.today().toordinal()
+    return date.fromordinal(random.randint(start, end))
+
 
 # create model classes here
 
@@ -40,6 +58,23 @@ class Donation(db.Model):
         for donation in donations:
             json_donations.append(donation.serialize)
         return json_donations
+
+    @staticmethod
+    def populate(n=20):
+        for _ in range(n):
+            added_by = MuUser.random().id
+            donation = Donation(
+                name=random_str(64),
+                email=random_str(64),
+                date=random_date(),
+                donation_source=random_str(128),
+                event=random_str(128),
+                num_tickets=random.randint(0, 1000),
+                added_by=added_by,
+                amount=random.randint(0, 1000),
+            )
+            db.session.add(donation)
+        db.session.commit()
 
     def __repr__(self):
         return "<Donation %r>" % self.id
@@ -109,6 +144,22 @@ class Project(db.Model):
             json_projects.append(i.serialize)
         return json_projects
 
+    @staticmethod
+    def populate(n=20):
+        for _ in range(n):
+            project = Project(
+                address=random_str(256),
+                city=random_str(64),
+                province=random_str(64),
+                postal_code=random_str(64),
+                neighbourhood=random_str(256),
+                name=random_str(64),
+                type=random_str(64),
+                is_completed=random_bool(),
+            )
+            db.session.add(project)
+        db.session.commit()
+
     def __repr__(self):
         return "<Project %r>" % self.id
 
@@ -171,6 +222,21 @@ class Contact(db.Model):
             json_contacts.append(c.serialize)
         return json_contacts
 
+    @staticmethod
+    def populate(n=20):
+        for _ in range(n):
+            contact = Contact(
+                name=random_str(256),
+                email=random_str(256),
+                secondary_email=random_str(256),
+                cellphone=random_str(256),
+                role=random_str(256),
+                organization=random_str(256),
+                neighbourhood=random_str(256),
+            )
+            db.session.add(contact)
+        db.session.commit()
+
     def __repr__(self):
         return "<Contact %r>" % self.id
 
@@ -198,6 +264,17 @@ class ContactType(db.Model):
         for contact_type in contact_types:
             json_contact_types.append(contact_type.serialize)
         return json_contact_types
+
+    @staticmethod
+    def populate(n=20):
+        for _ in range(n):
+            contact_type = ContactType(
+                hex_colour=random_str(8),
+                type=random_str(64),
+                description=random_str(512),
+            )
+            db.session.add(contact_type)
+        db.session.commit()
 
 
 # MuUser Domain Model Class
@@ -232,6 +309,20 @@ class MuUser(db.Model):
         for user in users:
             json_users.append(user.serialize)
         return json_users
+
+    @staticmethod
+    def populate(n=20):
+        for _ in range(n):
+            user = MuUser(
+                name=random_str(64), email=random_str(64), role=random_str(32)
+            )
+            db.session.add(user)
+        db.session.commit()
+
+    @staticmethod
+    def random():
+        users = MuUser.query.all()
+        return random.choice(users)
 
     def __repr__(self):
         return "<User %r>" % self.email
