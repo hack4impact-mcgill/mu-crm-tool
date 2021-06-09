3cf7d293afa619bed600ed80ae30fa357c2a49ea
CurtisMIT <41308927+CurtisMIT@users.noreply.github.com>
2021-06-09 13:06:35 -0400

    donor button (#174)

--- frontend/src/components/pages/donors/index.tsx
+++ frontend/src/components/pages/donors/index.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import Button from "@material-ui/core/Button";
+import { useAppDispatch } from "../../../redux/hooks";
+import { importCSV } from "./donorSlice";
 
 const DonorPage: React.FC = () => {
-  return <div>donors placeholder</div>;
+  const dispatch = useAppDispatch();
+  const onUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(importCSV(e.target.files!));
+  };
+
+  return (
+    <div>
+      <div>donor placeholder</div>
+      <Button variant="contained" color="primary" component="label">
+        Import CSV File
+        <input type="file" hidden multiple onChange={onUpload} />
+      </Button>
+    </div>
+  );
 };
 
 export default DonorPage;
